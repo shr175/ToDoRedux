b5d508b4bf0fd55fa576d03bdf12be1bdfd6b702
Shreya Nayak <shreyanayak@Shreyas-MacBook-Air.local>
2022-07-14 17:10:52 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,80 +1,26 @@
 
-import { useSelector,useDispatch } from 'react-redux';
-import react from 'react';
-import './App.css';
-import {actions} from './store';
+// import { useSelector,useDispatch } from 'react-redux';
+// import react from 'react';
+// import {actions} from './store';
+
 import Create from './components/Create';
 import Navbar from './components/Navbar';
+import './App.css';
 
 function App() {
-
-
-
 return (
 
   <>
+  
   <Navbar />
-  <div className="calling my-3">
   <Create />
-  </div>
+
  
   </>
-  
-  
+
   );
 
 
-
-
-
-  // const counter = useSelector((state) => state.counter);
-
-  // const dispatch = useDispatch();
-
-
-  // const increment = () => {
-  //   dispatch(actions.increment());
-  // };
-
-  // const decrement = () => {
-  //   dispatch(actions.decrement());
-  // };
-
-  // const addBy = () => {
-  //   dispatch(actions.addBy(10));
-  // };
-
-
-
-
-//<........simple way to write code........>
-
-
-  // const increment = () => {
-  //   dispatch({type:'INC'});
-  // };
-
-  // const decrement = () => {
-  //   dispatch({type:'DEC'});
-  // };
-
-  // const addBy = () => {
-  //   dispatch({type:'ADD',payload:10});
-  // };
-
-  //</....simple way to write code.....>
-
-
-
-  // return (
-  //   <div>
-  //    {/* <h1>To Do App</h1>
-  //    <h2>{counter}</h2>
-  //    <button onClick={increment}>Increment</button>
-  //    <button onClick={decrement}>Decrement</button>
-  //    <button onClick={addBy}>Add By 10</button> */}
-  //   </div>
-  // );
 }
 
 export default App;
--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,30 @@ const addToTaskReducer = (state = initialState,action) => {
         case 'submit_task': {
             return {tasks: [...state.tasks, action.payload]};
         }
+
+        case 'delete_task':{
+            const updatedTasks = []
+            state.tasks.forEach( (task) => 
+            {
+                // console.log("task",task)
+                if(task.id!==action.payload.id) {
+                updatedTasks.push(task)
+            //    console.log("inside if",updatedTasks);
+        } 
+    })
+            // console.log("payload ",action.payload);
+            // console.log(state.tasks);
+            // console.log("delete_task_reached",updatedTasks);
+            //comenting for better reach
+            return {tasks: updatedTasks}
+       
+             
+           }
+
+        case 'update_task':{
+            return {tasks: [...state.tasks[action.payload],action.payload]};
+        
+        } 
         default:
             return state;
     }
